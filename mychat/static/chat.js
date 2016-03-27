f368b89ecd6a66f99985e4a859bdd8fc06df7a2e
Kamal Mustafa <kamal.mustafa@gmail.com>
2016-03-27 09:37:09 +0800

    add commment on skipping own message

--- mychat/static/chat.js
+++ mychat/static/chat.js
@@ -36,6 +36,12 @@ var demo = new Vue({
             self.chatsock = new WebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
             self.chatsock.onmessage = function(message) {
                 var data = JSON.parse(message.data);
+                
+                // skip showing our own message as we already show it
+                // during sending. Probably we should do this on the backend
+                // and skip broadcasting the message in the first place.
+                // But that may require some way to exclude certain connection
+                // from the Group ... hmmm.
                 if (data['nickname'] == self.nickname) {
                     return;
                 } 
