1de5176bd87ae255fb9d060f5a041c1422cd289e
Kamal Mustafa <kamal.mustafa@gmail.com>
2016-03-27 12:02:53 +0800

    fix asgi

--- mychat/asgi.py
+++ mychat/asgi.py
@@ -1,5 +1,5 @@
 import os
 import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mychat.settings")
 channel_layer = channels.asgi.get_channel_layer()
